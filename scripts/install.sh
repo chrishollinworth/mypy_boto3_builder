fb66388370629b082ae97f82037285519becd999
Chris Hollinworth <chollinworth@gmail.com>
2025-05-08 09:54:35 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,18 +10,18 @@ fi
 
 if [[ "$1" == "main" ]]; then
     echo Installing boto3-stubs package
-    uv pip install ${OUTPUT_PATH}/boto3_stubs_package
+    pip install ${OUTPUT_PATH}/boto3_stubs_package
     exit
 fi
 
 if [[ "$1" == "full" ]]; then
     echo Installing boto3-stubs-full package
-    uv pip install ${OUTPUT_PATH}/boto3_stubs_full_package
+    pip install ${OUTPUT_PATH}/boto3_stubs_full_package
     exit
 fi
 
 for package in $PACKAGES
 do
     echo Installing $(basename ${package})
-    uv pip install ${package}
+    pip install ${package}
 done
